9a3bf18543012ce3622fb416f54983581b88798d
Samandar <samandartolybaev2@gmail.com>
2025-05-22 11:38:21 +0300

    fou

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,13 +1,12 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/organisms/Header";
-import { Frontend } from "../directions/Frontend"; 
+import { Frontend } from "../directions/Frontend";
 import { Backend } from "../directions/Backend";
 import { MobileDevelopment } from "../directions/MobileDevelopment";
 import { TD } from "../directions/TD";
 import { SisAdm } from "../directions/SisAdm";
 import { MechRob } from "../directions/MechRob";
-
 
 const Layout = () => {
   return (
@@ -18,12 +17,6 @@ const Layout = () => {
           <Outlet />
         </Suspense>
       </main>
-      <Frontend />
-      <Backend />
-      <MobileDevelopment />
-      <TD />
-      <SisAdm />
-      <MechRob />
     </div>
   );
 };
