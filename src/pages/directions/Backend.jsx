fdee59b783c24ab50c4da3472626dcbda4a97839
DanielRuslanov <{email}>
2025-05-27 14:06:16 +0600

    Обновлён компонент Layout

--- src/pages/directions/Backend.jsx
+++ src/pages/directions/Backend.jsx
@@ -1,7 +1,7 @@
 import DirectionPage from "../../components/molecules/DirectionPage";
 import { useTranslation } from "react-i18next";
 
-import img from "../../assets/img/backprogrammer.jpg";
+import img from "../../assets/img/back.jpg";
 import img2 from "../../assets/img/backend.jpg";
 import time from "../../assets/icon/time.svg";
 import money from "../../assets/icon/money.svg";
