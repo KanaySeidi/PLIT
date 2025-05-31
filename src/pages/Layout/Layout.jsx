58856ae9f7fc26a710fd27de1b93b8a2d61300d8
kirito <tilekovdekjan999@gmail.com>
2025-05-31 13:10:09 +0600

    applicants

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/organisms/Header";
-import Students from "../../pages/HeaderPages/Students";
 
 
 
@@ -14,7 +13,6 @@ const Layout = () => {
           <Outlet />
         </Suspense>
       </main>
-      <Students/>
     </div>
   );
 };
