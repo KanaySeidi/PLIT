490c388ee2493227ec4d31557a0821d12e7d7e16
Hanif <alibakiev4hanif@gmail.com>
2025-06-12 15:00:26 +0600

    pulling all stuff

--- src/stores/useAdminStore.js
+++ src/stores/useAdminStore.js
@@ -4,16 +4,10 @@ import axios from "axios";
 const API = "https://www.norussatours.com";
 
 const useAdminStore = create((set) => ({
-  stuff: [],
-  initialStaff: [],
-  accessToken: null,
-  kgTours: [],
-  centralAsiaTours: [],
-  tour: {},
-  polaroids: [],
-  gallery: [],
-  video: "",
+  courses: [],
+  course: {},
 
+  // Авторизация
   signIn: async (credentials) => {
     try {
       const response = await axios.post(`${API}/api/auth/sign-in`, credentials);
@@ -30,156 +24,63 @@ const useAdminStore = create((set) => ({
     localStorage.removeItem("token");
   },
 
-  addTour: async (data) => {
+  // Добавление курса
+  addCourse: async (data) => {
     try {
-      await axios.post(`${API}/api/edit/tours`, data, {
+      await axios.post(`${API}/api/courses`, data, {
         headers: { "Content-Type": "multipart/form-data" },
       });
     } catch (error) {
-      console.error("Error creating tour", error);
+      console.error("Error creating course:", error);
     }
   },
 
-  getKyrgyzstanTours: async () => {
+  // Получение всех курсов
+  getCourses: async () => {
     try {
-      const response = await axios.get(`${API}/api/edit/tours?countries=KG`);
-      set({ kgTours: response.data });
+      const response = await axios.get(`${API}/api/courses`);
+      set({ courses: response.data });
     } catch (error) {
-      console.log(error);
+      console.error("Error fetching courses:", error);
     }
   },
 
-  editTour: async (id, data) => {
+  // Получение одного курса
+  getCourse: async (id) => {
     try {
-      await axios.put(`${API}/api/edit/tours/${id}`, data, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-    } catch (error) {
-      console.error("Error creating tour", error);
-    }
-  },
-
-  getCentralAsiaTours: async () => {
-    try {
-      const response = await axios.get(
-        `${API}/api/edit/tours?countries=TJK,KZ,UZ,TM`
-      );
-      set({ centralAsiaTours: response.data });
-    } catch (error) {
-      return error;
-    }
-  },
-
-  getTour: async (id) => {
-    try {
-      const response = await axios.get(`${API}/api/edit/tours/${id}`);
-      set({ tour: response.data });
+      const response = await axios.get(`${API}/api/courses/${id}`);
+      set({ course: response.data });
       return response.data;
     } catch (error) {
-      return error;
+      console.error("Error fetching course:", error);
     }
   },
 
-  deleteTour: async (id) => {
-    await axios.delete(`${API}/api/edit/tours/${id}`);
-  },
-
-  postStuff: async (data) => {
+  // Редактирование курса
+  editCourse: async (id, data) => {
     try {
-      await axios.post(`${API}/api/stuff`, data);
-    } catch (error) {
-      return error;
-    }
-  },
-
-  getStuff: async () => {
-    try {
-      const response = await axios(`${API}/api/stuff`);
-      set({ stuff: response.data });
-    } catch (error) {}
-  },
-
-  deleteStaff: async (id) => {
-    try {
-      const response = await axios.delete(`${API}/api/stuff/${id}`);
-      return response.data;
-    } catch (error) {
-      return error;
-    }
-  },
-
-  getOneStaff: async (id) => {
-    try {
-      const res = await axios(`${API}/api/stuff/${id}`);
-      set({ initialStaff: res.data });
-    } catch (error) {
-      return error;
-    }
-  },
-
-  editStaff: async (id, data) => {
-    try {
-      await axios.put(`${API}/api/stuff/${id}`, data, {
+      await axios.put(`${API}/api/courses/${id}`, data, {
         headers: { "Content-Type": "multipart/form-data" },
       });
     } catch (error) {
-      console.log(error);
+      console.error("Error editing course:", error);
     }
   },
 
-  getPolaroids: async () => {
-    const response = await axios(`${API}/api/polaroid`);
-    set({ polaroids: response.data });
-  },
-
-  deletePolaroid: async (id) => {
+  // Удаление курса
+  deleteCourse: async (id) => {
     try {
-      await axios.delete(`${API}/api/polaroid/${id}`);
-    } catch (error) {}
-  },
-
-  uploadPolaroid: async (data) => {
-    try {
-      await axios.post(`${API}/api/polaroid`, data, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
+      await axios.delete(`${API}/api/courses/${id}`);
     } catch (error) {
-      console.error("Error creating tour", error);
+      console.error("Error deleting course:", error);
     }
   },
 
-  postGalleryImage: async (data) => {
-    await axios.post(`${API}/api/gallery`, data, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-  },
-
-  getGalleryImages: async () => {
-    const response = await axios(`${API}/api/gallery`);
-    set({ gallery: response.data });
-  },
-
-  deleteGalleryImage: async (id) => {
-    try {
-      await axios.delete(`${API}/api/gallery/${id}`);
-    } catch (error) {
-      return error;
-    }
-  },
-
-  postVideo: async (data) => {
-    await axios.post(`${API}/api/video`, data);
-  },
-
+  // Проверка авторизации
   isAdmin: (path, navigate) => {
     if (!localStorage.getItem("token")) {
       navigate(path);
     }
-  },
-
-  getVideo: async () => {
-    const response = await axios(`${API}/api/video`);
-    set({ video: response.data });
   },
 }));
 
