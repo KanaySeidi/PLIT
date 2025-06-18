789c1fe95768ae4704176b271a4b454a59e0c187
unknown <zeronovzeron950@gmail.com>
2025-06-18 14:15:33 +0600

    dastan docs

--- src/pages/HeaderPages/NpaKr.jsx
+++ src/pages/HeaderPages/NpaKr.jsx
@@ -20,22 +20,22 @@ const NpaKr = () => {
           {({ open }) => (
             <div className="p-6">
               <Disclosure.Button
-                onClick={() => handleToggle("webDeveloper")}
+                onClick={() => handleToggle("normativeActsKR")}
                 className="group flex w-full items-center justify-between border-b border-gray-300 pb-4"
               >
                 <span className="text-lg font-semibold bg-bordo text-white px-2 py-1 rounded transition-colors">
-                  {t("NPAKR.NPAKR")}
+                  {t("docsprograms.normativeActsKR")}
                 </span>
                 <motion.div
                   animate={{
-                    rotate: openSection === "webDeveloper" ? -180 : 0,
+                    rotate: openSection === "normativeActsKR" ? -180 : 0,
                   }}
                   transition={{ duration: 0.3 }}
                 >
                   <ChevronDownIcon className="h-8 w-8 text-gray-500 group-hover:text-blue-400" />
                 </motion.div>
               </Disclosure.Button>
-              {openSection === "webDeveloper" && (
+              {openSection === "normativeActsKR" && (
                 <Disclosure.Panel static>
                   <motion.div
                     initial={{ opacity: 0, y: -10 }}
@@ -45,45 +45,45 @@ const NpaKr = () => {
                     className="mt-4 p-4 border border-gray-300 rounded-lg bg-gray-50"
                   >
                     <p className="text-xl font-bold text-bordo text-center mb-4">
-                      {t("NPAKR.PLIT")}
+                      {t("docsdescriptions.ActsPLIT99")}
                     </p>
                     <div className="flex flex-col gap-2">
                       {[
                         {
                           href: "https://drive.google.com/file/d/16FzDn88xh1TJ-UJns9zl5m8fbUCfV8M2/view?usp=sharing",
-                          text: t("NPAKR.exam"),
+                          text: t("links.ActsExam"),
                         },
                         {
                           href: "https://docs.google.com/document/d/1e7dw6zgthgXeMOPCTjavjVtJNmfXMZ_m/edit?usp=sharing&ouid=101506021742149629101&rtpof=true&sd=true",
-                          text: t("NPAKR.PresidentialDecree"),
+                          text: t("links.PresidentialDecree"),
                         },
                         {
                           href: "https://docs.google.com/document/d/1Hh3XcqXCgPVmrfuvRipPqgraK9TkpXnX/edit?usp=sharing&ouid=101506021742149629101&rtpof=true&sd=true",
-                          text: t("NPAKR.GovernmentDecree"),
+                          text: t("links.GovernmentDecree"),
                         },
                         {
                           href: "https://docs.google.com/document/d/1pxg1mcwTlotTPgS70RXXk6AojLlvCjLO/edit?usp=sharing&ouid=101506021742149629101&rtpof=true&sd=true",
-                          text: t("NPAKR.CabinetOfMinisters"),
+                          text: t("links.CabinetOfMinisters"),
                         },
                         {
                           href: "https://docs.google.com/document/d/1VJyG1rW8LilDz8wjWzImp5MKnOXZycx7/edit?usp=sharing&ouid=101506021742149629101&rtpof=true&sd=true",
-                          text: t("NPAKR.IECRegulations"),
+                          text: t("links.IECRegulations"),
                         },
                         {
                           href: "https://drive.google.com/file/d/1GB9-J71BPT4jbXhWS0xqcMDb5bAnQ8dm/view?usp=sharing",
-                          text: t("NPAKR.KyrgyzStateEdDocs2025"),
+                          text: t("links.KyrgyzStateEdDocs2025"),
                         },
                         {
                           href: "https://drive.google.com/file/d/1fHcrqSCUTjkh-xuvtRx5tS7A98_iqp65/view?usp=sharing",
-                          text: t("NPAKR.StateStandardTemplate"),
+                          text: t("links.StateStandardTemplate"),
                         },
                         {
                           href: "https://drive.google.com/file/d/1npCkhvD166_ZJZwFoLmm6-Bx6sQ9ClsT/view?usp=sharing",
-                          text: t("NPAKR.IGA-25"),
+                          text: t("links.IGA-25"),
                         },
                         {
                           href: "https://docs.google.com/document/d/1eP2r35tZO01C8hrFX_Il8hf4SlAQSz8M/edit?usp=sharing&ouid=101506021742149629101&rtpof=true&sd=true",
-                          text: t("NPAKR.LawsOfTheKR"),
+                          text: t("links.LawsOfTheKR"),
                         },
                       ].map(({ href, text }) => (
                         <motion.a
